f982760a081712313047fd6d99a7f6e75d2dee40
AnniYuko <yuko99@outlook.de>
2022-02-26 22:28:02 +0100

    collision makes change in color

--- explore details/CollisionDetection.c
+++ explore details/CollisionDetection.c
@@ -1,5 +1,5 @@
 #include "raylib.h"
-#include <stdlib.h>
+#include <math.h>
 
 typedef struct s_circle{
     float x;
@@ -10,12 +10,16 @@ typedef struct s_circle{
 int main(void)
 {
     //initialize
-    const int screenWidth = 1200;
-    const int screenHeight = 800;
+    const int screenWidth = 1000;
+    const int screenHeight = 700;
     
-    InitWindow(screenWidth, screenHeight, "Circle Collision");
+    InitWindow(screenWidth, screenHeight, "Circles Collision");
     
+    //starting position
     t_circle moveCirc = { GetScreenWidth()/2, GetScreenHeight()/2, 40 };
+    t_circle staticCirc = { 300, 400, 80 };
+    
+    bool collision = false;
     
     SetTargetFPS(60);
     
@@ -26,12 +30,15 @@ int main(void)
         moveCirc.x = GetMouseX();
         moveCirc.y = GetMouseY();
         
+        //collision means distance between circles is smaller than the sum of the radii
+        collision = sqrt(pow(staticCirc.x - moveCirc.x, 2) + pow(staticCirc.y - moveCirc.y, 2)) <= (staticCirc.radius + moveCirc.radius);
+        
         BeginDrawing();
         
         ClearBackground(BLACK);
         
-        DrawCircle(300, 500, 80, WHITE);
-        DrawCircle(moveCirc.x, moveCirc.y, moveCirc.radius, SKYBLUE);
+        DrawCircle(staticCirc.x, staticCirc.y, staticCirc.radius, WHITE);
+        DrawCircle(moveCirc.x, moveCirc.y, moveCirc.radius, collision ? RED : SKYBLUE);
         
         EndDrawing();
     }
