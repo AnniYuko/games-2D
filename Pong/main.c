1f1103700f76253a4130481813bf1123283b6c7c
AnniYuko <yuko99@outlook.de>
2022-03-07 17:20:52 +0100

    added diff Screens (Title, Game, Play again)

--- Pong/main.c
+++ Pong/main.c
@@ -12,7 +12,7 @@ typedef struct s_player{
     Vector2 velo;
 }t_player;
 
-typedef enum SwitchScreen { START = 0, TUTORIAL, GAMEPLAY, END } SwitchScreen;
+typedef enum SwitchScreen { START = 0, TITLE, GAMEPLAY, END } SwitchScreen;
 
 Vector2 my_add_vectors2(Vector2 v1, Vector2 v2)
 {
@@ -41,7 +41,9 @@ int main(void)
     float playerHeight = 120;
     float playerWidth = 10;
     
-    InitWindow(screenWidth, screenHeight, "Ping Pong!");
+    int tutorial = 0;
+    
+    InitWindow(screenWidth, screenHeight, "Another Pong");
     
     t_player playerL = { 0 };
     playerL.pos = (Vector2) { 50, (screenHeight/2 - playerHeight/2)};
@@ -61,84 +63,131 @@ int main(void)
     //main game loop
     while (!WindowShouldClose())
     {
-        //add velocity to ball position
-        ball.pos = my_add_vectors2(ball.pos, ball.velo);
+                
+        switch(currScreen)
+        {
+            case START:
+            {
+              if (IsKeyPressed(KEY_ENTER))
+                    currScreen = TITLE;
+            } break;
+            case TITLE:
+            {
+                if (IsKeyPressed(KEY_T))
+                    tutorial = (!tutorial);
+                else if (IsKeyPressed(KEY_ENTER))
+                    currScreen = GAMEPLAY;
+            } break;
+            case GAMEPLAY:
+            {
+                //add velocity to ball position
+                ball.pos = my_add_vectors2(ball.pos, ball.velo);
 
-        //bounce off top and bottom of screen
-        if (ball.pos.y > (screenHeight - 5))
-        {
-            ball.pos.y = screenHeight - 5;
-            ball.velo.y *= -1;
-        }
-        else if (ball.pos.y < 5)
-        {
-            ball.pos.y = 5;
-            ball.velo.y *= -1;
-        }
-        //for testing purposes
-        /*if (ball.pos.x < 5)
-        {
-            ball.pos.x = 5;
-            ball.velo.x *= -1;
-        }
-        else if (ball.pos.x > (screenWidth - 5))
-        {
-            ball.pos.x = screenWidth - 5;
-            ball.velo.x *= -1;
-        }*/
+                //bounce off top and bottom of screen
+                if (ball.pos.y > (screenHeight - 5))
+                {
+                    ball.pos.y = screenHeight - 5;
+                    ball.velo.y *= -1;
+                }
+                else if (ball.pos.y < 5)
+                {
+                    ball.pos.y = 5;
+                    ball.velo.y *= -1;
+                }
+             
+                //move players up(-) and down(+)
+                if (IsKeyDown(KEY_F) && (playerL.pos.y > 0))
+                {
+                    playerL.pos = my_sub_vectors2(playerL.pos, playerL.velo);
+                }
+                else if (IsKeyDown(KEY_D) && (playerL.pos.y < (screenHeight - playerHeight)))
+                {
+                    playerL.pos = my_add_vectors2(playerL.pos, playerL.velo);
+                }
+                if (IsKeyDown(KEY_J) && (playerR.pos.y > 0))
+                {
+                    playerR.pos = my_sub_vectors2(playerR.pos, playerL.velo);
+                }
+                else if (IsKeyDown(KEY_K) && (playerR.pos.y < (screenHeight - playerHeight)))
+                {
+                    playerR.pos = my_add_vectors2(playerR.pos, playerR.velo);
+                }
         
-        //move players up(-) and down(+)
-        if (IsKeyDown(KEY_F) && (playerL.pos.y > 0))
-        {
-            playerL.pos = my_sub_vectors2(playerL.pos, playerL.velo);
-        }
-        else if (IsKeyDown(KEY_D) && (playerL.pos.y < (screenHeight - playerHeight)))
-        {
-            playerL.pos = my_add_vectors2(playerL.pos, playerL.velo);
-        }
-        if (IsKeyDown(KEY_J) && (playerR.pos.y > 0))
-        {
-            playerR.pos = my_sub_vectors2(playerR.pos, playerL.velo);
-        }
-        else if (IsKeyDown(KEY_K) && (playerR.pos.y < (screenHeight - playerHeight)))
-        {
-            playerR.pos = my_add_vectors2(playerR.pos, playerR.velo);
-        }
-        
-        //ball-player collision
-        if (CheckCollisionCircleRec(ball.pos, ball.rad, (Rectangle) { playerR.pos.x, playerR.pos.y, playerWidth, playerHeight}))
-        {    
-            if (ball.velo.x > 0)
-                ball.velo.x *= -1.05;
-        }
-        else if (CheckCollisionCircleRec(ball.pos, ball.rad, (Rectangle) { playerL.pos.x, playerL.pos.y, playerWidth, playerHeight}))
-        {
-            if (ball.velo.x < 0)
-            ball.velo.x *= -1.05;
-        }
+                //ball player collision & speed up game over time
+                if (CheckCollisionCircleRec(ball.pos, ball.rad, (Rectangle) { playerR.pos.x, playerR.pos.y, playerWidth, playerHeight}))
+                {    
+                    if (ball.velo.x > 0)
+                    {
+                        ball.velo.x *= -1.05;
+                        ball.velo.y *=  1.05;
+                    }
+                }
+                else if (CheckCollisionCircleRec(ball.pos, ball.rad, (Rectangle) { playerL.pos.x, playerL.pos.y, playerWidth, playerHeight}))
+                {
+                    if (ball.velo.x < 0)
+                    {
+                        ball.velo.x *= -1.05;
+                        ball.velo.y *=  1.05;
+                    }
+                }
 
-        if (ball.pos.x >= screenWidth)
-        {
-            scoreL++;
-            ServeBall(&ball);
+                if (ball.pos.x >= screenWidth)
+                {
+                    if (!tutorial)
+                        scoreL++;
+                    ServeBall(&ball);
+                }
+                else if (ball.pos.x <= 0)
+                {
+                    if (!tutorial)
+                        scoreR++;
+                    ServeBall(&ball);
+                }
+                
+                if (IsKeyPressed(KEY_ENTER))
+                    currScreen = END;
+            } break;
+            case END:
+            {
+                if (IsKeyPressed(KEY_ENTER))
+                    currScreen = GAMEPLAY;
+            } break;
+            default: break;
         }
-        else if (ball.pos.x <= 0)
-        {
-            scoreR++;
-            ServeBall(&ball);
-        }
-
+        //--------------------- Drawing ----------------------------------------
         BeginDrawing();
             ClearBackground(BLACK);
             
-            DrawCircleV(ball.pos, ball.rad, PURPLE);
-            DrawRectangle(playerL.pos.x, playerL.pos.y, playerWidth, playerHeight, WHITE);
-            DrawRectangle(playerR.pos.x, playerR.pos.y, playerWidth, playerHeight, WHITE);
+            switch(currScreen)
+            {
+                case START:
+                {
+                    DrawText(TextFormat("Let's play Pong!"), screenWidth/2 - 150, screenHeight/2 - 100, 40, RAYWHITE);
+                    DrawText(TextFormat("Press Enter to continue"), screenWidth/2 - 150, screenHeight/2 + 200, 25, BLUE);
+                } break;
+                case TITLE:
+                {
+                    DrawText(TextFormat("Press 'T' to try the tutorial first"), screenWidth/2 - 200, screenHeight/2, 25, tutorial ? GREEN : RAYWHITE);
+                    DrawText(TextFormat("Press Enter to continue"), screenWidth/2 - 150, screenHeight/2 + 200, 25, BLUE);
+                } break;
+                case GAMEPLAY:
+                {
+                    DrawCircleV(ball.pos, ball.rad, PURPLE);
+                    DrawRectangle(playerL.pos.x, playerL.pos.y, playerWidth, playerHeight, WHITE);
+                    DrawRectangle(playerR.pos.x, playerR.pos.y, playerWidth, playerHeight, WHITE);
             
-            DrawText(TextFormat("Score: %d", scoreL), 150, 120, 20, GREEN);
-            DrawText(TextFormat("Score: %d", scoreR), 950, 120, 20, GREEN);
+                    DrawText(TextFormat("Score: %d", scoreL), 150, 120, 20, GREEN);
+                    DrawText(TextFormat("Score: %d", scoreR), 950, 120, 20, GREEN);
 
-            DrawFPS(1100, 10);
+                    DrawFPS(1100, 10);
+                } break;
+                case END:
+                {
+                    DrawText(TextFormat("Press Enter to play again"), screenWidth/2 - 150, screenHeight/2 + 200, 25, BLUE);
+                } break;
+                default: break;
+            }
+            
         EndDrawing();
     }
     CloseWindow();
